9f0e2466836643cb2462211a764373dfa537a197
harigro <trierbank@gmail.com>
2025-05-15 22:11:35 +0700

    efisiensi algoritma untuk fungsi achord di modul achord

--- teori/achord.py
+++ teori/achord.py
@@ -30,26 +30,20 @@ def achord(note_tuts: List[str], tuts: str, q: List[str]) -> Tuple[str]:
     """
     Membuat akor berdasarkan array q dan tuts
     """
-    data = []
     rotasi = rotate_array(arr=note_tuts, valeu=tuts)
-    for i in q:
-        if i < len(rotasi):
-            data.append(rotasi[i])
-        else:
-           data.append(rotasi[ i % len(rotasi) ]) 
-    return data
+    return [ rotasi[a] if a < len(rotasi) else rotasi[ a % len(rotasi) ]  for a in q]
 
 
-if __name__ == "__main__":
-    NOTE_NAMES = ['C', 'C_Sharp', 'D', 'D_Sharp', 'E', 'F',
-                'F_Sharp', 'G', 'G_Sharp', 'A', 'A_Sharp', 'B']
+# if __name__ == "__main__":
+#     NOTE_NAMES = ['C', 'Db', 'D', 'Eb', 'E', 'F',
+#                 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 
-    data = gets_data_arr(NOTE_NAMES, 'A')
-    rotasi = rotate_left(NOTE_NAMES, data)
-    print(NOTE_NAMES)
-    print(data)
-    print(rotasi)
-    print()
-    print(rotate_array(NOTE_NAMES, 'D'))
-    print()
-    print(achord(note_tuts=NOTE_NAMES, tuts="C", q=[0, 4, 7, 21]))
+#     data = gets_data_arr(NOTE_NAMES, 'A')
+#     rotasi = rotate_left(NOTE_NAMES, data)
+#     print(NOTE_NAMES)
+#     print(data)
+#     print(rotasi)
+#     print()
+#     print(rotate_array(NOTE_NAMES, 'D'))
+#     print()
+#     print(achord(note_tuts=NOTE_NAMES, tuts="C", q=[0, 4, 7, 10, 14]))
