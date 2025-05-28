5f5d42952e45bf0b687a07d0a93b0fccf8d6522e
fstill12 <bkerja873@gmail.com>
2025-05-28 20:40:56 +0700

    menambah rumus tangga nada

--- teori/note.py
+++ teori/note.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 # Namedtuple untuk konfigurasi warna
-Config = namedtuple("Config", ["flat", "sharp"])
+Config = namedtuple("Config", ["flat", "sharp", "tangga_nada"])
 
 # Konstanta flat
 flat = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
@@ -9,5 +9,17 @@ flat = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 # Konstanta flat
 sharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
+rumus = {
+    "judul": "rumus_interval_tangga_nada",
+    "tangga_nada":{
+        "mayor": [2, 2, 1, 2, 2, 2, 1],
+        "minor": [2, 1, 2, 2, 1, 2, 2],
+        "minor_natural": [2, 1, 2, 2, 1, 2, 2],
+        "minor_harmonik": [2, 1, 2, 2, 1, 3, 1],
+        "minor_melodik": [2, 1, 2, 2, 2, 2, 1],
+        "kromatik": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    } 
+}
+
 # Definisi warna
-Note = Config(flat=flat, sharp=sharp)
+Note = Config(flat=flat, sharp=sharp, tangga_nada=rumus)
