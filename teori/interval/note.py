3e6fa7ff485d16229044c0f94b981b8305c1fd8b
fstill12 <bkerja873@gmail.com>
2025-05-31 00:28:38 +0700

    menghapus komentar yang tidak penting

--- teori/interval/note.py
+++ teori/interval/note.py
@@ -35,19 +35,6 @@ derajat = {
     6: "6 (submediant)",
     7: "7 (leading tone/subtonik)",
 }
-# Skala dasar dengan interval (dalam jumlah langkah setengah nada)
-# SCALE_PATTERNS = {
-#     "major": [2, 2, 1, 2, 2, 2, 1],
-#     "minor": [2, 1, 2, 2, 1, 2, 2],
-    # "harmonic minor": [2, 1, 2, 2, 1, 3, 1],
-    # "melodic minor": [2, 1, 2, 2, 2, 2, 1],
-    # "major pentatonic": [2, 2, 3, 2, 3],
-    # "minor pentatonic": [3, 2, 2, 3, 2],
-    # "blues": [3, 2, 1, 1, 3, 2],
-    # "chromatic": [1] * 12,
-    # "whole tone": [2] * 6
-# }
-
 
 # Definisi warna
 Note = Config(flat=flat, sharp=sharp, tangga_nada=rumus, derajat=derajat)
