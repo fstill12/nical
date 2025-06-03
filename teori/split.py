42b09420b7480e2c74a822e5778b54f425b5b329
fstill12 <bkerja873@gmail.com>
2025-06-04 00:41:50 +0700

    menambah kelas DictFactory

--- teori/split.py
+++ teori/split.py
@@ -1,11 +1,33 @@
 Intervals = dict[str, str]
 
 class SplitDict:
+    """
+    Kelas ini digunakan untuk mengelola interval sebagai dictionary."""
     def __init__(self, interval: Intervals):
         self.interval = interval
 
     def kunci(self):
+        """Mengembalikan daftar kunci dari interval."""
         return list(self.interval.keys())
     
     def nilai(self):
+        """Mengembalikan daftar nilai dari interval."""
         return list(self.interval.values())
+    
+    def __getitem__(self, key: str) -> str:
+        """Mengembalikan nilai dari interval berdasarkan kunci."""
+        return self.interval[key]
+
+class DictFactory:
+    """Kelas ini digunakan untuk membuat SplitDict dari interval yang diberikan."""
+    @staticmethod
+    def buat(interval: Intervals) -> SplitDict:
+        """Membuat SplitDict dari interval yang diberikan."""
+        return SplitDict(interval)
+
+    @staticmethod
+    def dari_list(intervals: list[tuple[str, str]]) -> SplitDict:
+        """Membuat SplitDict dari list tuple yang berisi interval."""
+        interval_dict = {k: v for k, v in intervals}
+        return DictFactory.buat(interval_dict)
+    
